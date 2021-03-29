2e75e46ebfc22ee5eba36d8541b896baf2298fe5
ayush_tibra <ayush.tibra@growisto.com>
2021-03-29 11:06:52 +0530

    add navigation to our menu itemsusing router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import HomePage from './homepage-component';
+import HomePage from './pages/homepage-component';
+
+import { Switch, Route } from 'react-router-dom';
+
+const HatsPage = () => <div>Hats Page</div>;
 
 function App() {
   return (
     <div className='App'>
-      <HomePage />
+      {/* <Switch> */}
+      <Route exact path='/' component={HomePage} />
+      <Route path='/hats' component={HatsPage} />
+      {/* </Switch> */}
+      {/* <HomePage /> */}
     </div>
   );
 }
---
+++ src/pages/homepage-component.jsx
@@ -0,0 +1,14 @@
+import React, { useState, useEffect } from 'react';
+import Directory from '../components/directory/directory.component';
+
+import './homepage.styles.scss';
+
+const HomePage = () => {
+  return (
+    <div className='homepage'>
+      <Directory />
+    </div>
+  );
+};
+
+export default HomePage;
